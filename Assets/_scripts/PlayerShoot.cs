6d6e1b4f9bc3f09a1994bc8dffd5aef4de9eb262
Ricky <nguyenkhanglhp1605@gmail.com>
2021-12-13 21:08:54 -0500

    Added gun model and shooting sound for player.

--- Assets/_scripts/PlayerShoot.cs
+++ Assets/_scripts/PlayerShoot.cs
@@ -6,12 +6,14 @@ public class PlayerShoot : MonoBehaviour
 {
     public Camera playerCamera;
     public GameObject bulletPrefab;
+    public AudioSource gunshotSound;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !Pause.GameIsPaused)
         {
+            gunshotSound.Play();
             GameObject bulletObject = Instantiate(bulletPrefab);
             bulletObject.transform.position = playerCamera.transform.position + playerCamera.transform.forward;
             bulletObject.transform.forward = playerCamera.transform.forward;
